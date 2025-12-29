e2c149e826b3cfc4ace9d2cb3993f8136a7a9291
Satnam Singh <166006254+satnam7777@users.noreply.github.com>
2025-12-29 12:00:03 +0530

    Enable CORS for all routes

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options('*', cors());
 
 // ----------------------------
 // ✅ Routes
